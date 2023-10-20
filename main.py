4e576579a197c15dd3915c4a6b4a3a299c5521e1
steveny1989 <steveny1989@gmail.com>
2023-10-20 19:48:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import openai, config, subprocess
 import numpy as np
 import os
 
-openai.api_key = "sk-RRmdAHyCG3E29ui7IECET3BlbkFJiXKwqLEBoqiTVu5RjgP"
+openai.api_key = "your-api-key"
 
 messages = [{"role": "system", "content": "You are a therapist."}]
 
